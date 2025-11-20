0848fa73ba9644ee11470c2de91d310b430e4293
nn <205666415+guessmesome@users.noreply.github.com>
2025-11-20 20:31:16 +0700

    upd

--- config.js
+++ config.js
@@ -1,8 +1,39 @@
-const CONFIG = {
-    products: {
-        1: 'https://vldmnt.ru/go/sdddk7uq7y',
-        2: 'https://trnsfx.ru/go/su71k51zx9',
-        3: 'https://trnsfx.ru/go/s89h285zev',
-        4: 'https://trnsfx.ru/go/sybokiosr4'
-    }
+const config = {
+    colors: {
+        primaryColor: "#2F65F0",
+        bgColor: "#F7F9FC",
+        cardBg: "#FFFFFF",
+        textSecondary: "#666666",
+        buttonHover: "#2554D0"
+    },
+    products: [
+        {
+            id: 1,
+            title: "Дебетовая карта",
+            subtitle: "Кэшбэк до 3000₽<br>в месяц",
+            image: "./images/Product_1.png",
+            url: "https://trnsfx.ru/go/s3wq40bpow"
+        },
+        {
+            id: 2,
+            title: "Кредитная карта",
+            subtitle: "Снятие наличных<br>без комиссии",
+            image: "./images/Product_2.png",
+            url: "https://trnsfx.ru/go/s0uusze41d"
+        },
+        {
+            id: 3,
+            title: "Кредит наличными",
+            subtitle: "До 90 дней без<br>первого платежа",
+            image: "./images/Product_3.png",
+            url: "https://trnsfx.ru/go/s2s0j4ozv5"
+        },
+        {
+            id: 4,
+            title: "Займ на карту",
+            subtitle: "Под выгодный<br>процент",
+            image: "./images/Product_4.png",
+            url: "https://trnsfx.ru/go/sh5ztmjczt"
+        }
+    ]
 };
